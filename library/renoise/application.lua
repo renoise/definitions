31869b63d4294cfa5467590f315bff21cefde2e2
taktik <taktik@renoise.com>
2024-02-22 15:45:57 +0100

    viewbuilder cleanups
    - add common header info
    - add class table definitions
    - use lua block code for examples
    - remove widget image placeholders
    - move renoise.Dialog to application

--- library/renoise/application.lua
+++ library/renoise/application.lua
@@ -290,3 +290,25 @@ function renoise.Application:save_instrument_sample(filename) end
 ---@param filename string
 ---@return boolean success
 function renoise.Application:save_theme(filename) end
+
+--------------------------------------------------------------------------------
+---## renoise.Dialog
+
+---A custom dialog created via the scripting API. Dialogs can be created
+---via `renoise.app():show_custom_dialog`.
+---
+---See `create custom views` on top of the renoise.ViewBuilder docs on how to
+---create views for the dialog.
+---@class renoise.Dialog
+---
+--- **READ-ONLY** Check if a dialog is alive and visible.
+---@field visible boolean
+renoise.Dialog = {}
+
+---### functions
+
+-- Bring an already visible dialog to front and make it the key window.
+function renoise.Dialog:show() end
+
+-- Close a visible dialog.
+function renoise.Dialog:close() end
