cd6860f2c204c31e32688fa9d42fafc51c1a4ff5
taktik <taktik@renoise.com>
2024-04-17 16:36:08 +0200

    add new "font", "align" and "secondary_color" properties to button

--- library/renoise/views/button.lua
+++ library/renoise/views/button.lua
@@ -21,11 +21,22 @@ error("Do not try to execute this file. It's just a type definition file.")
 ---button's background color of the theme.
 ---@alias ButtonBitmapPath BitmapImagePath
 
+---Setup the buttons text's or bitmap's alignment within the button.
+---@alias ButtonAlignment
+---| "left"   # aligned to the left
+---| "right"  # aligned to the right
+---| "center" # center (default)
+
 ---When set, the unpressed button's background will be drawn in the specified color.
 ---A text color is automatically selected unless explicitly set, to make sure its
 ---always visible.
 ---Set color {0,0,0} to enable the theme colors for the button again.
 ---@alias ButtonColor RGBColor
+
+---When set, the unpressed button's background text or bitmap will be drawn in the
+---specified color.
+---Set color {0,0,0} to enable the theme colors for the button again.
+---@alias ButtonSecondaryColor RGBColor
 
 ---Get/set the style a button should be displayed with.
 ---@alias ButtonStyle
@@ -49,7 +60,10 @@ error("Do not try to execute this file. It's just a type definition file.")
 ---@class renoise.Views.Button : renoise.Views.Control
 ---@field text ButtonLabel
 ---@field bitmap ButtonBitmapPath
+---@field align ButtonAlignment
+---@field font TextFontStyle
 ---@field color ButtonColor
+---@field secondary_color ButtonSecondaryColor
 ---@field style ButtonStyle
 local Button = {}
 
@@ -85,7 +99,10 @@ function Button:remove_released_notifier(notifier) end
 ---@class ButtonProperties : ControlProperties
 ---@field text ButtonLabel?
 ---@field bitmap ButtonBitmapPath?
+---@field align ButtonAlignment?
+---@field font TextFontStyle?
 ---@field color ButtonColor?
+---@field secondary_color ButtonColor?
 ---@field style ButtonStyle?
 ---@field notifier ButtonNotifier?
 ---@field pressed ButtonNotifier?
